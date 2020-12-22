55630c0fe3076aed8d70a4a598bfb2a5a93c290a
Yiyit <yigitk2524@gmail.com>
2020-12-22 22:59:57 +0300

    .

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup
 setup(
     name = 'projectidea-cli',
     version = '1.0',
+    author = 'Yigit K.',
+    author_email = 'projectideacli@gmail.com',
+    description = ('You can find different project ideas easily with this cli!'),
+    license = 'MIT',
     packages = ['projectidea'],
     entry_points = {
         'console_scripts': [
